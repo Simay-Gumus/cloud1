1c94f7adcad1f4a88afd85a1c879b6950035738d
Simay-Gumus <gumussimay534@gmail.com>
2025-04-06 23:19:26 +0300

    Delete frontend directory

--- frontend/src/entities/Users.tsx
+++
@@ -1,9 +0,0 @@
-import Comment from "./comments.tsx";
-
-export default interface User {
-    _id: string;
-    username: string;
-    password: string
-    isAdmin: boolean;
-    comments: Comment[];
-}
